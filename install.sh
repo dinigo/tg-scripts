c99a112ce3d73200adf0ea120425a446a8fe9ba6
Daniel I <demil133@gmail.com>
2014-04-25 12:48:58 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone https://github.com/vysheng/tg ~/telegram
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev
+cd ~/telegram
+./configure
+make
+sudo chown -R root:root ~/telegram
+sudo chmod -R 777 ~/telegram
+sudo mv ~/telegram /etc/
+sudo ln -s /etc/telegram/telegram /usr/bin/telegram
